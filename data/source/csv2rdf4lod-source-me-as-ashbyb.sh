1bf511c16a8eda3d0379110706e2c72eb7e1beef
Your Name <you@example.com>
2013-02-20 20:51:17 +0000

    During install: added stub directories and readme files.

---
+++ data/source/csv2rdf4lod-source-me-as-ashbyb.sh
@@ -0,0 +1,11 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+
+export CSV2RDF4LOD_CONVERT_PERSON_URI="http://tw.rpi.edu/instances/BrendanAshby"
+alias semmdd='sudo su semmdd'
+export PATH=$PATH`/home/ashbyb/opt/prizms/bin/install/paths.sh`
+export CLASSPATH=$CLASSPATH`/home/ashbyb/opt/prizms/bin/install/classpaths.sh`
+export CSV2RDF4LOD_HOME="/home/ashbyb/opt/prizms/repos/csv2rdf4lod-automation"
+export JENAROOT=/home/ashbyb/opt/apache-jena-2.7.4
