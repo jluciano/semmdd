31a321de9a0dfcf0ffbb3cc4bbe36de8c1275849
Joanne S. Luciano <jluciano@rpi.edu>
2013-02-22 15:46:23 +0000

    During install: added stub directories and readme files.

---
+++ data/source/csv2rdf4lod-source-me-as-jluciano.sh
@@ -0,0 +1,10 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+
+export CSV2RDF4LOD_CONVERT_PERSON_URI="http://tw.rpi.edu/instances/JoanneLuciano"
+export PATH=$PATH`/home/jluciano/opt/prizms/bin/install/paths.sh`
+export CLASSPATH=$CLASSPATH`/home/jluciano/opt/prizms/bin/install/classpaths.sh`
+export CSV2RDF4LOD_HOME="/home/jluciano/opt/prizms/repos/csv2rdf4lod-automation"
+export JENAROOT=/home/jluciano/opt/apache-jena-2.7.4
